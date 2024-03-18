6d791a96747eeb2a0382f640d3873ec1318de626
SCARLET-SARAH <162133775+SCARLET-SARAH@users.noreply.github.com>
2024-03-19 00:26:15 +0300

    create index.js

--- index.js
+++ index.js
@@ -2,4 +2,79 @@ const currentUser = 'Grace Hopper';
 
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = 'Welcome to Flatbook, ';
+const welcomeMessage = 'Welcome to Flatbook,';
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = currentUser;
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, currentUser';
+
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+
+"const currentUser = 'Grace Hopper';".length;
+//=> 35
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+shortGreeting
+  contains "Welcome, "
+
+const shortGreeting = 'Welcome, ';
+
+const currentUser = 'Grace Hopper';
+
+...
+
+const shortGreeting = `Welcome, ${currentUser}`;
+
+'Edsger Dijkstra'[0];
+//=> "E"
+
+'Edsger Dijkstra'.charAt(0);
+//=> "E"
+
+'Edsger Dijkstra'.length;
+//=> 15
+
+'Edsger Dijkstra'[15];
+//=> undefined
+
+'Edsger Dijkstra'[14];
+//=> "a"
+'Edsger Dijkstra'.slice();
+//=> "Edsger Dijkstra"
+'Edsger Dijkstra'.slice(7);
+//=> "Dijkstra"
+'Edsger Dijkstra'.slice(0, 3);
+//=> "Eds"
+
+currentUser.slice(0, 1);
+
+const currentUser = 'Grace Hopper';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
